7268db7b138853d7056be148609a26f5ad47bc88
anmolsidmol <anmol.sidmol@gmail.com>
2022-07-14 22:36:53 +0530

    steering

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    float steerSpeed = 0.1f;
+
+    [SerializeField]
+    float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
